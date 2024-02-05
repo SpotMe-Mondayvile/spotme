1d199c773eab1ab32f9df8655239358dc7d0b229
Magus Chihaba <chihabam@gmail.com>
2024-02-04 23:54:13 -0600

    For compatibility purposed i switched our build tool to maven, using pom.xml

---
+++ spotme-rest/src/main/java/com/mts/spotmerest/SpotMeRestApplication.java
@@ -0,0 +1,13 @@
+package com.mts.spotmerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotMeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotMeRestApplication.class, args);
+	}
+
+}
---
+++ spotme-rest/src/main/java/com/mts/spotmerest/SpotmeRestApplication.java
@@ -0,0 +1,13 @@
+package com.mts.spotmerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotmeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotmeRestApplication.class, args);
+	}
+
+}
