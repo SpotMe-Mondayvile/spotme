330c93703e6a3bf5ba5389688da1f5df56262ce6
SOULROSEGOLD <116165703+SOULROSEGOLD@users.noreply.github.com>
2024-03-29 22:39:17 -0500

    #41 routine controller,

---
+++ spotme-rest/src/main/java/com/mts/spotmerest/mappers/RoutineDAO.java
@@ -0,0 +1,27 @@
+package com.mts.spotmerest.mappers;
+
+import com.mts.spotmerest.models.Routine;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface RoutineDAO
+        extends JpaRepository<Routine, Long>//gives CRUD capabilities to routine
+{     //when pulling from DB. assuming we would
+    ///for routineDAO, consider whether we need to specify by userID number
+
+
+    void deleteByName(String routineName);
+
+    Optional<Routine> findByRoutineName(String routineName);
+
+
+
+
+
+}
