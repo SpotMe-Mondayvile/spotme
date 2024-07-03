1c1a79fc06ef096fc72c818b83699a987e7738b8
chihabam <chihabam@gmail.com>
2024-07-03 17:50:42 -0500

    Changed Backend Security filter chain

--- spotme-rest/src/main/java/com/mts/spotmerest/configs/SecurityConfig.java
+++ spotme-rest/src/main/java/com/mts/spotmerest/configs/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
+                .cors(cors -> cors.disable())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
