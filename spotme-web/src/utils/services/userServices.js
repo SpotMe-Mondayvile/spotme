f47802f746ec8f2e47bd9fac91d7a944600dbd9f
chihabam <chihabam@gmail.com>
2024-07-29 23:43:10 -0500

    prep branch for pr

--- spotme-web/src/utils/services/userServices.js
+++ spotme-web/src/utils/services/userServices.js
@@ -7,3 +7,10 @@ export function signup(user){
 export function userLogin(creds){
     return apiClient.post('/v1/auth/authenticate',creds)
 }
+
+export function userGetInfo(){
+    return apiClient.get('/v1/user/getinfo')
+}
+export function userGetEmail(email){
+    return apiClient.get(`/v1/user/email/${email}`)
+}
