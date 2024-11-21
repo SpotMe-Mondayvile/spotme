c1d53a4c6e80e112a456e1089d2c431cfe83841a
devincohen <devin94cohen@gmail.com>
2024-11-21 00:30:08 -0600

    added infinite scroll and pull-down refresh

--- spotme-web/src/pages/home/Home.tsx
+++ spotme-web/src/pages/home/Home.tsx
@@ -4,7 +4,10 @@ import { IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, I
   IonList,
   IonItem,
   IonAvatar,
-  IonLabel } from '@ionic/react';
+  IonLabel,
+  IonRefresher,
+  IonRefresherContent,
+  RefresherEventDetail } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import './Home.css';
 import SideNav from '../../components/menu/SideNav';
@@ -15,20 +18,39 @@ import NavMenu from '../sidenav/NavMenu';
 const Home: React.FC = () => {
   const navigate = useIonRouter()
     
-  useEffect(()=>{
-    checkLogin(navigate)
-  },[])
+  // useEffect(()=>{
+  //   checkLogin(navigate)
+  // },[])
   
   const [items, setItems] = useState<string[]>([]);
 
-  useEffect(() => {
+  const generateItems = () => {
     const newItems = [];
-    for (let i = 1; i < 51; i++) {
-      newItems.push(`Item ${items.length + i}`);
+    for (let i = 0; i < 50; i++) {
+      newItems.push(`Item ${1 + items.length + i}`);
     }
     setItems([...items, ...newItems]);
+  };
+
+  const resetItems = () => {
+    setItems([]);
+    generateItems();
+  }
+
+  useEffect(() => {
+    const newItems = [];
+    generateItems();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  
+  function handleRefresh(event: CustomEvent<RefresherEventDetail>) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      resetItems()
+      event.detail.complete();
+    }, 2000);
+  }
 
   return (
     <>
@@ -52,17 +74,22 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">Content
-        <IonList>
-          {items.map((item, index) => (
-            <IonItem key={item}>
-              <IonAvatar slot="start">
-                <img src={'https://picsum.photos/80/80?random=' + index} alt="avatar" />
-              </IonAvatar>
-              <IonLabel>{item}</IonLabel>
-            </IonItem>
-          ))}
-      </IonList>
-          <IonInfiniteScroll>
+          <IonRefresher slot="fixed"  onIonRefresh={handleRefresh}>
+            <IonRefresherContent></IonRefresherContent>
+          </IonRefresher>
+          <IonList>
+            {items.map((item, index) => (
+              <IonItem key={item}>
+                <IonAvatar slot="start">
+                  <img src={'https://picsum.photos/80/80?random=' + index} alt="avatar" />
+                </IonAvatar>
+                <IonLabel>{item}</IonLabel>
+              </IonItem>
+            ))}
+          </IonList>
+          <IonInfiniteScroll onIonInfinite={(ev) => {generateItems();
+                                                      setTimeout(() => ev.target.complete(), 500);
+          }}>
             <IonInfiniteScrollContent loadingText="Please wait..." loadingSpinner="bubbles">
               scroll
             </IonInfiniteScrollContent>
