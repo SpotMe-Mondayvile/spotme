362d658218343224eb180d4d3f1442371c55d90d
devincohen <devin94cohen@gmail.com>
2024-11-21 00:48:26 -0600

    minor fix

--- spotme-web/src/pages/home/Home.tsx
+++ spotme-web/src/pages/home/Home.tsx
@@ -38,6 +38,7 @@ const Home: React.FC = () => {
   }
 
   useEffect(() => {
+    checkLogin(navigate)
     const newItems = [];
     generateItems();
     // eslint-disable-next-line react-hooks/exhaustive-deps
