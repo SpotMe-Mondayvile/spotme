ccd3c8dbf99ad78c4638a3c434b8acfe6a74e684
devincohen <devin94cohen@gmail.com>
2024-11-20 23:55:00 -0600

    starting

--- spotme-web/src/pages/home/Home.tsx
+++ spotme-web/src/pages/home/Home.tsx
@@ -1,8 +1,14 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonRouter,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonLabel } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import './Home.css';
 import SideNav from '../../components/menu/SideNav';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { checkLogin } from './../../utils/userSession';
 import NavMenu from '../sidenav/NavMenu';
 
@@ -12,6 +18,17 @@ const Home: React.FC = () => {
   useEffect(()=>{
     checkLogin(navigate)
   },[])
+  
+  const [items, setItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newItems = [];
+    for (let i = 1; i < 51; i++) {
+      newItems.push(`Item ${items.length + i}`);
+    }
+    setItems([...items, ...newItems]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -34,7 +51,23 @@ const Home: React.FC = () => {
             <IonTitle>SpotMe</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent className="ion-padding">Content</IonContent>
+        <IonContent className="ion-padding">Content
+        <IonList>
+          {items.map((item, index) => (
+            <IonItem key={item}>
+              <IonAvatar slot="start">
+                <img src={'https://picsum.photos/80/80?random=' + index} alt="avatar" />
+              </IonAvatar>
+              <IonLabel>{item}</IonLabel>
+            </IonItem>
+          ))}
+      </IonList>
+          <IonInfiniteScroll>
+            <IonInfiniteScrollContent loadingText="Please wait..." loadingSpinner="bubbles">
+              scroll
+            </IonInfiniteScrollContent>
+          </IonInfiniteScroll>
+        </IonContent>
       </IonPage>
     </>
   );
